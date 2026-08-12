68207cb96304354d858adb8c30bde6aa9f84b100
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-12 22:46:45 +0600

    till now

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+const port = 4000;
+
+//middlewares
+app.use(cors());
+app.use(clerkMiddleware());
+app.use(express.json({limit: "20mb"}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+
+//DB
+
+// Routes
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+});
+
+app.listen(port,()=> {
+    console.log(`server started on http://localhost:${port}`);
+    
+})
