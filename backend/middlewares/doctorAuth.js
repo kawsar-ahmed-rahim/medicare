c47471769f82bfd8c2af4219645e853cb9a23766
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-14 00:19:39 +0600

    till now

--- backend/middlewares/doctorAuth.js
+++ backend/middlewares/doctorAuth.js
@@ -15,8 +15,31 @@ export default async function doctorAuth(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     try {
-        
+        const payload = jwt.verify(token, JWT_SECRET);
+
+        if(payload.role && payload.role !== "doctor"){
+            return res.status(403).json({
+                success: false,
+                message: "Access Denied(not a doctor)"
+            })
+        }
+        // fetch doctor
+        const doctor = await Doctor.findById(payload.id).select("-password");
+
+        if(!doctor) {
+            return res.status(401).json({
+                success: false,
+                message: "Doctor not found"
+            })
+        }
+        // attach doctor to request
+        req.doctor = doctor;
+        next();
     } catch (error) {
-        
+        console.error("Doctor JWT Verification failed: ", error);
+        return res.status(401).json({
+            success: false,
+            message: "Token invalid or missing or expired"
+        })
     }
 }
---
+++ backend/middlewares/multer.js
@@ -0,0 +1,38 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// multer setup
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir),
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(
+            null, uniqueName + path.extname(file.originalname)
+        )
+    }
+})
+
+// file filter
+
+const fileFilter = (req,file,cb) =>{
+        if(
+            file.mimetype === "image/png" || 
+            file.mimetype === "image/jpg" ||
+            file.mimetype === "image/jpeg" ||
+            file.mimetype === "image/webp" 
+        ){
+            cb(null, true);
+        } else {
+            cb(new Error("Only image files are allowed"), false);
+        }
+}
+
+// multer config
