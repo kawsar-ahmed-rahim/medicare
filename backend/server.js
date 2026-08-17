3ed28b6eaf977b356ce2e5a67dbbd70f820aa541
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-17 13:52:59 +0600

    till now

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import {connectDB} from './config/db.js';
 import doctorRouter from './routes/doctorRouter.js';
 import serviceRouter from './routes/serviceRouter.js';
+import appointmentRouter from './routes/appointmentRouter';
 
 const app = express();
 const port = 4000;
@@ -22,6 +23,7 @@ connectDB();
 // Routes
 app.use("/api/doctors", doctorRouter);
 app.use("/api/services", serviceRouter);
+app.use("/api/appointments", appointmentRouter);
 
 app.get('/',(req,res)=>{
     res.send("API WORKING")
