73115b0b3b5df9c182d57ca0e2c816b2c69cc6c4
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-18 20:02:40 +0600

    till now

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { navbarStyles as ns } from "../assets/dummyStyles.js";
 import logoImg from "../assets/logo.png";
-
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <header className={ns.header}>
@@ -8,6 +8,9 @@ const Navbar = () => {
         <div className={ns.flexContainer}>
           <div className={ns.logoContainer}>
             <img src={logoImg} alt="img" srcset="" className={ns.logoImage} />
+            <Link to="/">
+            <div className={ns.logoLink}>Medicare</div>
+            </Link>
           </div>
         </div>
       </nav>
