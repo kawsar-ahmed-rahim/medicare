d94e1d51bc86a7484df7a51c62d3c5a32aaee765
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-27 21:44:00 +0600

    till now

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,7 @@ import Appointments from "./pages/Appointments";
 import SerDashboard from "./pages/SerDashboard";
 import AddSer from "./pages/AddSer";
 import ListService from "./pages/ListService";
+import ServiceAppointments from "./pages/ServiceAppointments";
 
 function RequireAuth({ children }) {
   const { isLoaded, isSignedIn } = useUser();
@@ -90,6 +91,14 @@ const App = () => {
         element={
           <RequireAuth>
             <ListService />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/service-appointments"
+        element={
+          <RequireAuth>
+            <ServiceAppointments />
           </RequireAuth>
         }
       />
