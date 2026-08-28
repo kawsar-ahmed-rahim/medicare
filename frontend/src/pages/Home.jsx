bf1c2a2a2576039fa86c1aa6176b7c34293b0b8f
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-29 02:50:30 +0600

    till now

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
+import Certification from '../components/Certification'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Certification />
     </div>
   )
 }
