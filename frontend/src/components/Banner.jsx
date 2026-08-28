97071e16233054063ca8308bcf2b0233c58333e1
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-08-29 02:13:25 +0600

    Banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,18 @@
 import { bannerStyles as a } from "./../assets/dummyStyles";
-// import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Calendar, Clock, Ribbon, ShieldUser, Star, Stethoscope, User } from "lucide-react";
+import banner from "../assets/BannerImg.png";
+import {
+  Calendar,
+  Clock,
+  Phone,
+  Ribbon,
+  ShieldUser,
+  Star,
+  Stethoscope,
+  User,
+} from "lucide-react";
 const Banner = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className={a.bannerContainer}>
       <div className={a.mainContainer}>
@@ -28,9 +37,9 @@ const Banner = () => {
                   {/* stars */}
                   <div className={a.starsContainer}>
                     <div className={a.starsInner}>
-                        {[1,2,3,4,5].map((star)=> (
-                            <Star className={a.starIcon} key={star} />
-                        ))}
+                      {[1, 2, 3, 4, 5].map((star) => (
+                        <Star className={a.starIcon} key={star} />
+                      ))}
                     </div>
                   </div>
                 </div>
@@ -38,34 +47,56 @@ const Banner = () => {
               {/* tagline */}
               <p className={a.tagline}>
                 Premium Healthcare
-                <span className={`block ${a.taglineHighlight}`}>At Your Fingertips</span>
+                <span className={`block ${a.taglineHighlight}`}>
+                  At Your Fingertips
+                </span>
               </p>
               <div className={a.featuresGrid}>
                 <div className={`${a.featureItem} ${a.featureBorderGreen}`}>
-                    <Ribbon className={a.featureIcon} />
-                    <span className={a.featureText}>Certified Specialists</span>
+                  <Ribbon className={a.featureIcon} />
+                  <span className={a.featureText}>Certified Specialists</span>
                 </div>
                 <div className={`${a.featureItem} ${a.featureBorderBlue}`}>
-                    <Clock className={a.featureIcon} />
-                    <span className={a.featureText}>24/7 Available</span>
+                  <Clock className={a.featureIcon} />
+                  <span className={a.featureText}>24/7 Available</span>
                 </div>
                 <div className={`${a.featureItem} ${a.featureBorderEmerald}`}>
-                    <ShieldUser className={a.featureIcon} />
-                    <span className={a.featureText}>safe &amp; Secure</span>
+                  <ShieldUser className={a.featureIcon} />
+                  <span className={a.featureText}>safe &amp; Secure</span>
                 </div>
                 <div className={`${a.featureItem} ${a.featureBorderPurple}`}>
-                    <User className={a.featureIcon} />
-                    <span className={a.featureText}>500+ Doctors</span>
+                  <User className={a.featureIcon} />
+                  <span className={a.featureText}>500+ Doctors</span>
                 </div>
               </div>
-              <div className={a.ctaButtonsContainer}><button onClick={()=> navigate("/doctors")} className={a.bookButton}>
-              <div className={a.bookButtonOverlay}></div>
-              <div className={a.bookButtonContent}>
-              <Calendar className={a.bookButtonIcon} />
-              <span>Book Appointment Now</span>
+              <div className={a.ctaButtonsContainer}>
+                <button
+                  onClick={() => navigate("/doctors")}
+                  className={a.bookButton}
+                >
+                  <div className={a.bookButtonOverlay}></div>
+                  <div className={a.bookButtonContent}>
+                    <Calendar className={a.bookButtonIcon} />
+                    <span>Book Appointment Now</span>
+                  </div>
+                </button>
+                <button
+                  className={a.emergencyButton}
+                  onClick={() => (window.location.href = "tel:123456789")}
+                >
+                  <div className={a.emergencyButtonContent}>
+                    <Phone className={a.emergencyButtonIcon} />
+                    <span>Emergency Call</span>
+                  </div>
+                </button>
               </div>
-            
-              </button></div>
+            </div>
+            <div className={a.rightImageSection}>
+              <div className={a.imageContainer}>
+                <div className={a.imageFrame}>
+                  <img src={banner} alt="banner" className={a.image} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
